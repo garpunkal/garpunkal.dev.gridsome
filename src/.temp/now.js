1fbe42145c2e60fa69bcf0bee377f916e5b649a8
Gareth Wright <garpunkal@gmail.com>
2021-01-19 14:54:53 +0000

    json experiences

--- src/.temp/now.js
+++ src/.temp/now.js
@@ -1,1 +1,1 @@
-export default 1610889384435
+export default 1611068089238
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,19 @@
     <Profile />
     <Projects />
     <Twitter />
-    <Experience />
+
+    <section class="text-white bg-gray-900 body-font">
+      <div class="container px-5 py-5 md:py-24 mx-auto">
+        <div
+          class="pt-0 pb-10 sm:items-start mx-auto"
+          v-for="edge in $page.posts.edges"
+          :key="edge.node.id"
+        >
+          <Experience :item="edge.node" />
+        </div>
+      </div>
+    </section>
+
     <SiteFooter />
   </Layout>
 </template>
@@ -45,3 +57,42 @@ export default {
   },
 };
 </script>
+<page-query>
+query {
+   posts: allExperiences(sortBy: "id", order: ASC) {
+    edges {
+      node {
+        id
+        title
+        job
+        location       
+        logo {
+          url			   
+			    background
+			    alt
+        }
+        url
+        shortUrl
+        from
+        to
+        isCurrent
+        description
+        projects {
+          title
+          items {
+            name
+            url
+          }
+        }       
+        contributions {
+           title
+          items {
+            name
+            url
+          }
+        }
+      }
+    }
+  }
+}
+</page-query>
