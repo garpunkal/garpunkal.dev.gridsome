bfd9283f05d678b5d35f96c23c9ab95bf5dc5e7d
Gareth Wright <garpunkal@gmail.com>
2021-03-23 19:12:51 +0000

    Id

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
 
     <section
       class="text-gray-500 bg-brand-dark dark:text-gray-300 dark:bg-gray-900 body-font"
-      id="articles"
+      id="content"
     >
       <div class="container px-5 py-5 md:py-14 mx-auto">
         <div class="flex w-full flex-wrap mb-20">
