be5189593e17f973692ca70db3b324611430d340
Gareth Wright <garpunkal@gmail.com>
2021-03-24 09:36:23 +0000

    Delete src/.temp directory

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - garpunkal.dev",
-  "siteUrl": "https://garpunkal.dev",
-  "version": "0.7.23",
-  "catchLinks": true
-}
--- src/.temp/plugins-server.js
+++
@@ -1,8 +0,0 @@
-import plugin_gridsome_plugin_gtm_5 from "J:\\git\\github.com\\garpunkal\\garpunkal.github.io\\node_modules\\gridsome-plugin-gtm\\gridsome.client.js"
-
-export default [
-  {
-    run: plugin_gridsome_plugin_gtm_5,
-    options: {"id":"GTM-54WJV5","enabled":true,"debug":false}
-  }
-]
