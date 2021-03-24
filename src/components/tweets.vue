d46ea73a8b88ac51fd122d5959d50c7470b0398e
Gareth Wright <garpunkal@gmail.com>
2021-03-24 12:53:53 +0000

    update twitter headline

--- src/components/tweets.vue
+++ src/components/tweets.vue
@@ -5,11 +5,19 @@
   >
     <div class="container px-5 py-5 md:py-14 mx-auto">
       <div class="flex w-full flex-wrap mb-20">
-        <h3
-          class="text-3xl font-extrabold sm:text-65xl tracking-tighter lg:text-4xl text-gray-600 dark:text-gray-500 text-center w-full mt-10 lg:mt-0"
+        <a
+          class="w-full"
+          href="https://twitter.com/garpunkal"
+          target="_blank"
+          rel="noopener"
+          title="Twitter"
         >
-          Tweets
-        </h3>
+          <h3
+            class="text-3xl font-extrabold sm:text-65xl tracking-tighter lg:text-4xl text-gray-600 dark:text-gray-500 text-center w-full mt-10 lg:mt-0"
+          >
+            Tweets
+          </h3>
+        </a>
       </div>
       <div class="flex w-full flex-wrap justify-center px-10 mt-10 md:mt-0">
         <Tweet />
