c5c432e392afdfa7ad240e86a5f84b6b5e501818
Gareth Wright <garpunkal@gmail.com>
2021-10-23 16:54:53 +0100

    updated shadow

--- src/components/profile.vue
+++ src/components/profile.vue
@@ -4,7 +4,7 @@
   >
     <div
       id="profile"
-      class="w-full lg:w-3/5 rounded-lg md:rounded-r-lg lg:rounded-l-lg xl:rounded-r-none shadow-2xl bg-white dark:bg-brand-dark dark:text-gray-300 mx-6 lg:mx-0"
+      class="w-full lg:w-3/5 rounded-lg md:rounded-r-lg lg:rounded-l-lg xl:rounded-r-none shadow-2xl-black bg-white dark:bg-brand-dark dark:text-gray-300 mx-6 lg:mx-0"
     >
       <div class="p-4 md:p-12 text-center lg:text-left">
         <ProfileImageSmall />
