b757bd0b1029f6318e1c088e11f0c28e4383334d
Gareth Wright <garpunkal@gmail.com>
2022-03-30 15:32:44 +0100

    updated project image

--- src/components/project.vue
+++ src/components/project.vue
@@ -3,78 +3,25 @@
     <a
       v-if="item.url"
       :href="item.url"
+      :title="item.title"
       target="_blank"
       rel="noopener"
-      :title="item.title"
     >
-      <figure class="relative">
-        <img
-          v-if="item.position === 'left top'"
-          :alt="item.image.alt"
-          :title="item.image.alt"
-          :src="item.image.url"
-          class="
-            w-full
-            h-full
-            block
-            rounded-lg
-            lg:rounded-l-lg
-            object-cover object-left-top
-            dark:brightness-50 dark:hover:brightness-100
-            border-brand
-            dark:border-brand-dark
-            border-2
-            md:border-3
-          "
-          :width="isLarge()? 615:300"
-          :height="isLarge()? 340:165"
-          loading="lazy"
-        />
-        <img
-          v-else
-          :alt="item.image.alt"
-          :title="item.image.alt"
-          :src="item.image.url"
-          class="
-            w-full
-            h-full
-            block
-            rounded-lg
-            lg:rounded-l-lg
-            object-cover object-top
-            dark:brightness-50 dark:hover:brightness-100
-            border-brand
-            dark:border-brand-dark
-            border-2
-            md:border-3
-          "
-             :width="isLarge()? 615:300"
-          :height="isLarge()? 340:165"
-          loading="lazy"
-        />
-        <figcaption
-          class="
-            absolute
-            left-0
-            bottom-0
-            px-4
-            py-1
-            bg-brand
-            dark:bg-brand-dark
-            text-xs text-gray-200
-            dark:text-gray-400
-            rounded-bl-lg rounded-tr-lg
-          "
-        >
-          {{ item.title }}
-        </figcaption>
-      </figure>
+      <ProjectImage :item="item" :large="large" />
     </a>
+    <div v-if="!item.url">
+      <ProjectImage :item="item" :large="large" />
+    </div>
   </div>
 </template>
             
 <script>
+import ProjectImage from "@/components/projectImage.vue";
+
 export default {
+  components: {
+    ProjectImage,
+  },
   props: {
     item: {
       type: Object,
@@ -86,10 +33,10 @@ export default {
     },
   },
   methods: {
-    isLarge: function()  {
+    isLarge: function () {
       return this.large;
-    }
-  }, 
+    },
+  },
 };
 </script>
          
