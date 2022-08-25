144c3f0741ab018d3ff86c778fc16437006896e7
Gareth Wright <garpunkal@gmail.com>
2022-08-25 12:46:33 +0100

    updated to ring

--- src/components/projectImage.vue
+++ src/components/projectImage.vue
@@ -5,7 +5,7 @@
 			:alt="item.image.alt"
 			:title="item.image.alt"
 			:src="item.image.url"
-			class="w-full h-full block rounded-lg lg:rounded-l-lg object-fit object-left-top dark:brightness-50 dark:hover:brightness-100 border-brand dark:border-brand-dark border-2 md:border-4"
+			class="w-full h-full block rounded-lg lg:rounded-l-lg object-fit object-left-top dark:brightness-50 dark:hover:brightness-100 ring-brand dark:ring-brand-dark ring-2 md:ring-4"
 			:width="isLarge() ? 615 : 300"
 			:height="isLarge() ? 340 : 165"
 			loading="lazy"
@@ -15,12 +15,12 @@
 			:alt="item.image.alt"
 			:title="item.image.alt"
 			:src="item.image.url"
-			class="w-full h-full block rounded-lg lg:rounded-l-lg object-fit object-top dark:brightness-50 dark:hover:brightness-100 border-brand dark:border-brand-dark border-2 md:border-4"
+			class="w-full h-full block rounded-lg lg:rounded-l-lg object-fit object-top dark:brightness-50 dark:hover:brightness-100 ring-brand dark:ring-brand-dark ring-2 md:ring-4"
 			:width="isLarge() ? 615 : 300"
 			:height="isLarge() ? 340 : 165"
 			loading="lazy"
 		/>
-		<figcaption class="absolute left-0 bottom-0 px-4 py-1 bg-brand dark:bg-brand-dark text-xs text-gray-200 dark:text-gray-400 rounded-bl-lg rounded-tr-lg ">
+		<figcaption class="absolute left-0 bottom-0 md:-left-1 md:-bottom-1 px-2 md:px-4 py-1 md:py-2 bg-brand dark:bg-brand-dark text-xs text-gray-200 dark:text-gray-400 rounded-bl-lg rounded-tr-lg ">
 			{{ item.title }}
 		</figcaption>
 	</figure>
--- src/components/projectImageAward.vue
+++ src/components/projectImageAward.vue
@@ -5,7 +5,7 @@
 			:alt="item.image.alt"
 			:title="item.image.alt"
 			:src="item.image.url"
-			class="w-full h-full block rounded-lg lg:rounded-l-lg object-fit object-left-top dark:brightness-50 dark:hover:brightness-100 border-brand-gold dark:border-brand-dark border-2 md:border-4 shadow-2xl-gold dark:shadow-2xl-brand"
+			class="w-full h-full block rounded-lg lg:rounded-l-lg object-fit object-left-top dark:brightness-50 dark:hover:brightness-100 ring-brand-gold dark:ring-brand-dark ring-2 md:ring-4 shadow-2xl-gold dark:shadow-2xl-brand"
 			:width="isLarge() ? 615 : 300"
 			:height="isLarge() ? 340 : 165"
 			loading="lazy"
@@ -15,12 +15,12 @@
 			:alt="item.image.alt"
 			:title="item.image.alt"
 			:src="item.image.url"
-			class="w-full h-full block rounded-lg lg:rounded-l-lg object-fit object-top dark:brightness-50 dark:hover:brightness-100 border-brand-gold dark:border-brand-dark border-2 md:border-4 shadow-2xl-gold dark:shadow-2xl-brand"
+			class="w-full h-full block rounded-lg lg:rounded-l-lg object-fit object-top dark:brightness-50 dark:hover:brightness-100 ring-brand-gold dark:ring-brand-dark ring-2 md:ring-4 shadow-2xl-gold dark:shadow-2xl-brand"
 			:width="isLarge() ? 615 : 300"
 			:height="isLarge() ? 340 : 165"
 			loading="lazy"
 		/>
-		<figcaption class="absolute left-0 bottom-0 px-4 py-1 bg-brand-gold dark:bg-brand-dark text-xs text-black dark:text-gray-400 rounded-bl-lg rounded-tr-lg ">
+		<figcaption class="absolute left-0 bottom-0 md:-left-1 md:-bottom-1 px-2 md:px-4 py-1 md:py-2 bg-brand-gold dark:bg-brand-dark text-xs text-black dark:text-gray-400 rounded-bl-lg rounded-tr-lg ">
 			<span class="hidden lg:inline-block"><strong>Award Winner</strong>:</span> {{ item.title }}
 		</figcaption>
 	</figure>
