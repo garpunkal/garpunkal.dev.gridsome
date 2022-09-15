ddb8f5debae77224ee5a1198f61627c9e7a84fbc
Gareth Wright <garpunkal@gmail.com>
2022-09-15 08:35:07 +0100

    updated profile images to be squidex

--- src/components/profileimage.vue
+++ src/components/profileimage.vue
@@ -1,8 +1,15 @@
 <template>
 	<div class="w-full lg:w-2/5">
 		<picture>
-			<source type="image/webp" srcset="@/assets/images/profiles/profile.webp" />
-			<img src="@/assets/images/profiles/profile.jpg" class="rounded-none lg:rounded-lg shadow-2xl-black hidden lg:block dark:brightness-50 dark:hover:brightness-100 ring-brand-dark dark:ring-brand ring-4 " alt="Gareth Wright" width="960" height="960" title="Gareth Wright" />
+			<source type="image/webp" srcset="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?cache=5184000&amp;format=WEBP" />
+			<img
+				src="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?cache=5184000"
+				class="rounded-none lg:rounded-lg shadow-2xl-black hidden lg:block dark:brightness-50 dark:hover:brightness-100 ring-brand-dark dark:ring-brand ring-4 "
+				alt="Gareth Wright"
+				width="960"
+				height="960"
+				title="Gareth Wright"
+			/>
 		</picture>
 	</div>
 </template>
--- src/components/profileimagesmall.vue
+++ src/components/profileimagesmall.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center">
-		<picture>
-			<source type="image/webp" srcset="@/assets/images/profiles/profile.webp" />
-			<img src="@/assets/images/profiles/profile.jpg"
+	<picture>
+			<source type="image/webp" srcset="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?cache=5184000&amp;format=WEBP" />
+			<img src="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?cache=5184000"
 			 class="w-full object-cover h-full object-center block rounded-full dark:brightness-50 dark:hover:brightness-100 filter-grayscale
 			 shadow-xl-black 
 			 ring-brand-dark dark:ring-brand ring-4"
