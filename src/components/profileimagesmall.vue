2d016e968e443b558827b6096294f68c2d1e5dda
Gareth Wright <garpunkal@gmail.com>
2022-10-13 08:25:26 +0100

    update images

--- src/components/profileimagesmall.vue
+++ src/components/profileimagesmall.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center">
 	<picture>
-			<source type="image/webp" srcset="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?width=192&amp;height=192&amp;cache=5184000&amp;format=WEBP" />
-			<img src="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?width=192&amp;height=192&amp;cache=5184000"
+			<source type="image/webp" srcset="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?width=192&height=192&cache=5184000&format=WEBP" />
+			<img src="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?width=192&height=192&cache=5184000"
 			 class="w-full object-cover h-full object-center block rounded-full dark:brightness-50 dark:hover:brightness-100 filter-grayscale
 			 shadow-xl-black 
 			 ring-brand-dark dark:ring-brand ring-4"
