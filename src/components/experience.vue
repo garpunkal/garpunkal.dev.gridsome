d205da55470d2c747fa775c07c4404d652752c0a
Gareth Wright <garpunkal@gmail.com>
2022-10-17 23:14:42 +0100

    projects

--- src/components/experience.vue
+++ src/components/experience.vue
@@ -22,7 +22,7 @@
 				<img :alt="item.logo.alt" :src="item.logo.url" :style="{ backgroundColor: item.logo.background }" class="rounded-lg lg:rounded-l-lg flex object-contain dark:brightness-50 dark:hover:brightness-100 w-100pixel h-100pixel ring-brand dark:ring-brand-dark ring-4" height="100" loading="lazy" :title="item.logo.alt" />
 			</picture>
 		</div>
-		<div class="md:pl-6 col-span-3 mt-6 sm:mt-0 md:pr-5 xl:border-r-2 xl:border-brand xl:border-opacity-25 pb-10 ">
+		<div class="md:pl-6 col-span-3 mt-6 sm:mt-0 md:pr-5 pb-10 ">
 			<h3 class=" text-white dark:text-brand mb-1 text-xl md:text-2xl font-bold">
 				{{ item.job }}
 			</h3>
@@ -46,32 +46,28 @@
 			</p>
 			<div class="mt-2 inline-block py-1 px-1 text-gray-400 dark:text-gray-500 text-xs" v-if="GetMonths(item.from, item.to) != '0 months'">{{ GetMonths(item.from, item.to) }}</div>
 			<div class="mt-5 font-smaller text-sm md:pl-5 dark:text-gray-300" v-html="item.description" v-if="item.description !== '<p>-</p>'" v-show="!item.hideDescription"></div>
-		</div>
-		<div class="md:pl-2 mt-0 sm:mt-0 pb-10 w-full flex flex-wrap content-start" v-if="item.projects.title || item.contributions.title">
-			<div class="w-full md:w-1/2 xl:w-full md:px-4">
+		
+		
+			
+			<div class="mt-10 md:pl-2 pb-10 w-full flex flex-wrap content-start" v-if="item.projects.title || item.contributions.title">
+			<div class="w-full md:w-1/2 ">
 				<div class="font-smaller mb-1 text-sm font-bold dark:text-gray-400 " v-if="item.projects.title">
 					{{ item.projects.title }}
 				</div>
-				<ul class=" list-outside font-smaller text-sm pl-5 list-none space-y-1 mt-3 break-words mb-6 ">
+				<ul class=" list-outside font-smaller text-sm list-none space-y-1 mt-3 break-words mb-6 md:columns-2 xl:columns-3 px-3 ">
 					<li v-for="project in item.projects.items" v-bind:key="project.name">
 						<a v-if="project.url" rel="noopener" :href="project.url" target="_blank" :title="project.name" class="inline-flex items-center">{{ project.name }}</a>
 						<span v-else class="dark:text-gray-400">{{ project.name }}</span>
 					</li>
-				</ul>
-			</div>
-
-			<div :class="item.contributions.title ? `w-full md:w-1/2 xl:w-full md:px-4 md:border-l-2 md:border-brand md:border-opacity-25 xl:border-none` : `w-full md:w-1/2 xl:w-full md:px-4 `">
-				<div  class=" font-smaller mb-1 text-sm font-bold dark:text-gray-400 " v-if="item.contributions.title">
-					{{ item.contributions.title }}
-				</div>
-				<ul  class=" list-outside font-smaller text-sm pl-5 list-none space-y-1 mt-3 break-words mb-6 ">
 					<li v-for="contribution in item.contributions.items" v-bind:key="contribution.name">
 						<a v-if="contribution.url" rel="noopener" :href="contribution.url" target="_blank" :title="contribution.name" class="inline-flex items-center">{{ contribution.name }}</a>
 						<span v-else class="dark:text-gray-400">{{ contribution.name }}</span>
 					</li>
-				</ul>
+					</ul>
 			</div>
 		</div>
+		</div>
+	
 	</div>
 </template>
 <script>
