82f7f4d23d5615b8aac2a041e93b88008073e896
Gareth Wright <garpunkal@gmail.com>
2022-10-18 09:45:31 +0100

    updated experiences

--- src/components/experience.vue
+++ src/components/experience.vue
@@ -1,53 +1,62 @@
 <template>
-	<div class="flex flex-wrap flex-row w-full md:pl-6 pl-0 text-gray-300">
-		<div class="flex flex-col align-start flex-wrap md:pl-6 sm:mt-0 items-start mb-5">
-			<a v-if="item.url" :href="item.url" target="_blank" rel="noopener" :title="item.title">
-				<picture>
+	<div class="w-full md:pl-6 pl-0 text-gray-300">
+	
+		<div class="w-full justify-center flex flex-row flex-wrap">
+			<div class="flex flex-row flex-wrap w-full md:w-auto md:pl-6 sm:mt-0 b-5">
+				<a v-if="item.url" :href="item.url" target="_blank" rel="noopener" :title="item.title">
+					<picture>
+						<source type="image/webp" :srcset="item.logo.webp" />
+						<img
+							:alt="item.logo.alt"
+							:src="item.logo.url"
+							:style="{ backgroundColor: item.logo.background }"
+							class=" rounded-lg lg:rounded-l-lg flex object-contain dark:brightness-50 dark:hover:brightness-100 w-100pixel h-100pixel ring-brand dark:ring-brand-dark ring-4"
+							width="100"
+							height="100"
+							loading="lazy"
+							:title="item.logo.alt"
+						/>
+					</picture>
+				</a>
+				<picture v-if="!item.url">
 					<source type="image/webp" :srcset="item.logo.webp" />
 					<img
 						:alt="item.logo.alt"
 						:src="item.logo.url"
 						:style="{ backgroundColor: item.logo.background }"
-						class=" rounded-lg lg:rounded-l-lg flex object-contain dark:brightness-50 dark:hover:brightness-100 w-100pixel h-100pixel ring-brand dark:ring-brand-dark ring-4"
-						width="100"
+						class="rounded-lg lg:rounded-l-lg flex object-contain dark:brightness-50 dark:hover:brightness-100 w-100pixel h-100pixel ring-brand dark:ring-brand-dark ring-4"
 						height="100"
 						loading="lazy"
 						:title="item.logo.alt"
 					/>
 				</picture>
-			</a>
+			</div>
+			<div class="flex flex-col flex-wrap w-full md:w-8/12 lg:w-5/12 md:pl-6 mt-6 md:mt-0 md:pr-5 pb-10">
+				<h3 class=" text-white dark:text-brand mb-1 text-xl md:text-2xl font-bold">
+					{{ item.job }}
+				</h3>
+				<h4 class="font-medium text-gray-300 mb-1 text-lg md:text-lg">
+					{{ item.title }}
+				</h4>
+				<h5 class="font-smaller  mb-1 text-sm dark:text-gray-300" v-if="item.url">
+					{{ item.location }}
+				</h5>
+				<h5 class="font-smaller  mb-5 text-sm dark:text-gray-300" v-else>
+					{{ item.location }}
+				</h5>
+				<p class="font-smaller  mb-5 text-sm" v-if="item.url">
+					<a :href="item.url" target="_blank" rel="noopener" class="inline-flex items-center" :title="item.title">{{ item.shortUrl }}</a>
+				</p>
+				<p class="font-smaller text-sm ">
+					<span class="text-sm inline-block px-4 py-2 my-1 rounded text-white bg-gray-800 dark:text-gray-500 dark:bg-gray-800 uppercase last:mr-0">{{ GetMonthYear(item.from) }}</span>
+					-
+					<span class=" text-sm inline-block px-4 py-2 my-1 rounded text-white bg-brand dark:text-gray-300 dark:bg-brand uppercase last:mr-0" v-if="item.isCurrent">CURRENT</span>
+					<span class=" text-sm inline-block px-4 py-2 my-1 rounded text-white bg-gray-800 dark:text-gray-500 dark:bg-gray-800 uppercase last:mr-0 " v-else>{{ GetMonthYear(item.to) }}</span>
+				</p>
+				<div class="mt-2 inline-block py-1 px-1 text-gray-400 dark:text-gray-500 text-xs" v-if="GetMonths(item.from, item.to) != '0 months'">{{ GetMonths(item.from, item.to) }}</div>
+				<div class="mt-5 font-smaller text-sm md:pl-5 dark:text-gray-300" v-html="item.description" v-if="item.description !== '<p>-</p>'" v-show="!item.hideDescription"></div>
 
-			<picture v-if="!item.url">
-				<source type="image/webp" :srcset="item.logo.webp" />
-				<img :alt="item.logo.alt" :src="item.logo.url" :style="{ backgroundColor: item.logo.background }" class="rounded-lg lg:rounded-l-lg flex object-contain dark:brightness-50 dark:hover:brightness-100 w-100pixel h-100pixel ring-brand dark:ring-brand-dark ring-4" height="100" loading="lazy" :title="item.logo.alt" />
-			</picture>
-		</div>
-		<div class="flex flex-col flex-wrap w-full xl:w-10/12 md:pl-6 mt-6 sm:mt-0 md:pr-5 pb-10">
-			<h3 class=" text-white dark:text-brand mb-1 text-xl md:text-2xl font-bold">
-				{{ item.job }}
-			</h3>
-			<h4 class="font-medium text-gray-300 mb-1 text-lg md:text-lg">
-				{{ item.title }}
-			</h4>
-			<h5 class="font-smaller  mb-1 text-sm dark:text-gray-300" v-if="item.url">
-				{{ item.location }}
-			</h5>
-			<h5 class="font-smaller  mb-5 text-sm dark:text-gray-300" v-else>
-				{{ item.location }}
-			</h5>
-			<p class="font-smaller  mb-5 text-sm" v-if="item.url">
-				<a :href="item.url" target="_blank" rel="noopener" class="inline-flex items-center" :title="item.title">{{ item.shortUrl }}</a>
-			</p>
-			<p class="font-smaller text-sm ">
-				<span class="text-sm inline-block px-4 py-2 my-1 rounded text-white bg-gray-800 dark:text-gray-500 dark:bg-gray-800 uppercase last:mr-0">{{ GetMonthYear(item.from) }}</span>
-				-
-				<span class=" text-sm inline-block px-4 py-2 my-1 rounded text-white bg-brand dark:text-gray-300 dark:bg-brand uppercase last:mr-0" v-if="item.isCurrent">CURRENT</span>
-				<span class=" text-sm inline-block px-4 py-2 my-1 rounded text-white bg-gray-800 dark:text-gray-500 dark:bg-gray-800 uppercase last:mr-0 " v-else>{{ GetMonthYear(item.to) }}</span>
-			</p>
-			<div class="mt-2 inline-block py-1 px-1 text-gray-400 dark:text-gray-500 text-xs" v-if="GetMonths(item.from, item.to) != '0 months'">{{ GetMonths(item.from, item.to) }}</div>
-			<div class="mt-5 font-smaller text-sm md:pl-5 dark:text-gray-300" v-html="item.description" v-if="item.description !== '<p>-</p>'" v-show="!item.hideDescription"></div>
-
-			<div class="mt-10 pb-10 w-full flex flex-wrap content-start flex-row"  v-if="item.projects.title || item.contributions.title">
+				<!-- <div class="mt-10 pb-10 w-full flex flex-wrap content-start flex-row"  v-if="item.projects.title || item.contributions.title">
 				<div class="w-full"  v-if="item.projects.title">
 					<div class="font-smaller text-sm font-bold mb-2 " v-if="item.projects.title">
 						{{ item.projects.title }}
@@ -75,6 +84,7 @@
 						</div>
 					</div>
 				</div>
+			</div> -->
 			</div>
 		</div>
 	</div>
