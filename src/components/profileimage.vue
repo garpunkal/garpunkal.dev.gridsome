366c80af885daba83a03bbd501a0e53207e87a09
Gareth Wright <garpunkal@gmail.com>
2022-11-20 19:12:10 +0000

    updated image size

--- src/components/profileimage.vue
+++ src/components/profileimage.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="w-full lg:w-2/5">
 		<picture>
-			<source type="image/webp" srcset="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?width=460&height=460&cache=31536000&format=WEBP" />
+			<source type="image/webp" srcset="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?width=690&height=690&cache=31536000&format=WEBP" />
 			<img
-				src="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?width=460&height=460&cache=31536000"
+				src="https://cloud.squidex.io/api/assets/garpunkaldev/dff63e1a-c889-4178-8ef3-1cee6efa814b/profile.jpg?width=690&height=690&cache=31536000"
 				class="hidden rounded-none lg:rounded-lg shadow-2xl-black lg:block dark:brightness-50 dark:hover:brightness-100 ring-brand-dark dark:ring-brand ring-4 "
 				alt="Gareth Wright"
-				width="460"
-				height="460"
+				width="690"
+				height="690"
 				title="Gareth Wright"				
 			/>
 		</picture>	
