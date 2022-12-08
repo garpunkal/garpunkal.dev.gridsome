0d91ee8815499d8e9cf84277f6b14ff503acd39b
Gareth Wright <garpunkal@gmail.com>
2022-12-08 15:01:54 +0000

    experience updates

--- src/components/experience.vue
+++ src/components/experience.vue
@@ -1,91 +1,34 @@
 <template>
-	<div class="flex flex-row flex-wrap justify-center w-full text-gray-300 mb-14 ring-brand">	
-		<div class="flex flex-row flex-wrap justify-center w-full gap-5 ">			
-			<div class="flex flex-row flex-wrap justify-start w-auto mt-2 ">
-				<a v-if="item.url" :href="item.url" target="_blank" rel="noopener" :title="item.title">
-					<picture>
-						<source type="image/webp" :srcset="item.logo.webp" />
-						<img
-							:alt="item.logo.alt"
-							:src="item.logo.url"
-							:style="{ backgroundColor: item.logo.background }"
-							class="flex object-contain rounded-lg lg:rounded-l-lg dark:brightness-50 dark:hover:brightness-100 w-[100px] h-[100px] ring-brand dark:ring-brand-dark ring-4"
-							width="100"
-							height="100"
-							loading="lazy"
-							:title="item.logo.alt"
-						/>
-					</picture>
-				</a>
-				<picture v-if="!item.url">
+	<div class="flex flex-row flex-wrap w-full">
+		<div class="flex justify-center w-full mb-2 rounded-lg ring-brand dark:ring-brand-dark ring-4 dark:brightness-50 dark:hover:brightness-100  " :style="`background-color:` + item.logo.background + `;background-size:auto;`">		
+			<a v-if="item.url" :href="item.url" target="_blank" rel="noopener" :title="item.title" class="w-full h-full flex justify-center">
+				<picture>
 					<source type="image/webp" :srcset="item.logo.webp" />
-					<img
-						:alt="item.logo.alt"
-						:src="item.logo.url"
-						:style="{ backgroundColor: item.logo.background }"
-						class="flex object-contain rounded-lg lg:rounded-l-lg dark:brightness-50 dark:hover:brightness-100 w-[100px] h-[100px] ring-brand dark:ring-brand-dark ring-4"
-						width="100"
-						height="100"
-						loading="lazy"
-						:title="item.logo.alt"
-					/>
+					<img :alt="item.logo.alt" :src="item.logo.url" :style="{ backgroundColor: item.logo.background }" class="flex object-contain rounded-lg lg:rounded-l-lg w-[100px] h-[100px]" width="100" height="100" loading="lazy" :title="item.logo.alt" />
 				</picture>
-			</div>
-			<div class="flex flex-col flex-wrap w-full text-center">
-				<h3 class="mb-1 text-xl font-bold text-white dark:text-brand">
+			</a>
+			<picture v-if="!item.url">
+				<source type="image/webp" :srcset="item.logo.webp" />
+				<img :alt="item.logo.alt" :src="item.logo.url" :style="{ backgroundColor: item.logo.background }" class="flex object-contain rounded-lg lg:rounded-l-lg  w-[100px] h-[100px] " width="100" height="100" loading="lazy" :title="item.logo.alt" />
+			</picture>		
+		</div>
+		<div class=" h-full w-full mt-2">
+			<div class="w-full ">
+				<h3 class="mb-1 text-xl font-bold text-white dark:text-gray-400">
 					{{ item.job }}
 				</h3>
-				<h4 class="mb-1 text-lg font-medium text-gray-300 dark:text-gray-400">
+				<h4 class="mb-1 text-lg text-gray-500 dark:text-gray-500">
 					{{ item.title }}
 				</h4>
-				<p class="mb-5 text-sm font-smaller" v-if="item.url">
-					<a :href="item.url" target="_blank" rel="noopener" class="inline-flex items-center" :title="item.title">{{ item.shortUrl }}</a>
-				</p>
-				<h5 class="mb-1 text-sm font-smaller dark:text-gray-400" v-if="item.url">
-					{{ item.location }}
-				</h5>
-				<h5 class="mb-5 text-sm font-smaller dark:text-gray-400" v-else>
-					{{ item.location }}
-				</h5>
-				
-				<p class="text-sm font-smaller ">
-					<span class="inline-block px-4 py-2 my-1 text-sm text-white uppercase bg-gray-800 rounded dark:text-gray-500 dark:bg-gray-800 last:mr-0">{{ GetMonthYear(item.from) }}</span>
-					-
+			</div>
+			<div class="w-full">	
+				<p class="text-sm font-smaller text-white ">
 					<span class="inline-block px-4 py-2 my-1 text-sm text-white uppercase rounded bg-brand dark:text-gray-300 dark:bg-brand last:mr-0" v-if="item.isCurrent">CURRENT</span>
 					<span class="inline-block px-4 py-2 my-1 text-sm text-white uppercase bg-gray-800 rounded dark:text-gray-500 dark:bg-gray-800 last:mr-0" v-else>{{ GetMonthYear(item.to) }}</span>
+					-
+					<span class="inline-block px-4 py-2 my-1 text-sm text-white uppercase bg-gray-800 rounded dark:text-gray-500 dark:bg-gray-800 last:mr-0">{{ GetMonthYear(item.from) }}</span>
 				</p>
-				<div class="inline-block px-1 py-1 mt-2 text-xs text-gray-400 dark:text-gray-500" v-if="GetMonths(item.from, item.to) != '0 months'">{{ GetMonths(item.from, item.to) }}</div>
-				<div class="mt-5 text-sm font-smaller md:pl-5 dark:text-gray-300" v-html="item.description" v-if="item.description !== '<p>-</p>'" v-show="!item.hideDescription"></div>
-
-				<!-- <div class="flex flex-row flex-wrap content-start w-full pb-10 mt-10"  v-if="item.projects.title || item.contributions.title">
-				<div class="w-full"  v-if="item.projects.title">
-					<div class="mb-2 text-sm font-bold font-smaller " v-if="item.projects.title">
-						{{ item.projects.title }}
-					</div>
-					<div class="mb-2 text-sm font-bold border-b-2 opacity-25 font-smaller border-brand " v-if="item.projects.title"></div>
-					<div class="grid w-full grid-cols-1 gap-2 text-sm font-smaller md:grid-cols-2 xl:grid-cols-3">
-						<div v-for="project in item.projects.items" v-bind:key="project.name">
-							
-							<a v-if="project.url" rel="noopener" :href="project.url" target="_blank" :title="project.name" class="inline-flex items-center">{{ project.name }}</a>
-							<span v-else class="dark:text-gray-400">{{ project.name }}</span>
-						</div>
-						
-					</div>
-				</div>
-		
-				<div class="w-full mt-8" v-if="item.contributions.title">
-					<div class="mb-2 text-sm font-bold font-smaller " v-if="item.contributions.title">
-						{{ item.contributions.title }}
-					</div>
-					<div class="mb-2 text-sm font-bold border-b-2 opacity-25 font-smaller border-brand " v-if="item.contributions.title"></div>
-					<div class="grid w-full grid-cols-1 gap-2 text-sm font-smaller md:grid-cols-2 xl:grid-cols-3">					
-						<div v-for="contribution in item.contributions.items" v-bind:key="contribution.name">
-							<a v-if="contribution.url" rel="noopener" :href="contribution.url" target="_blank" :title="contribution.name" class="inline-flex items-center">{{ contribution.name }}</a>
-							<span v-else class="dark:text-gray-400">{{ contribution.name }}</span>
-						</div>
-					</div>
-				</div>
-			</div> -->
+				<div class="inline-block px-1 py-1 text-xs text-gray-400 dark:text-gray-500" v-if="GetMonths(item.from, item.to) != '0 months'">{{ GetMonths(item.from, item.to) }}</div>				
 			</div>
 		</div>
 	</div>
--- src/components/experiences.vue
+++ src/components/experiences.vue
@@ -1,13 +1,16 @@
 <template>
 	<section class=" bg-brand-dark dark:bg-gray-900 body-font" id="experience">
-		<div class="container px-5 py-5 md:py-14 mx-auto">
-			<div class="flex w-full flex-wrap mb-20">
-				<h3 class="text-3xl font-extrabold sm:text-65xl tracking-tighter lg:text-4xl  text-center w-full mt-10 lg:mt-0 text-gray-400  dark:text-gray-300">
+		<div class="container px-5 py-5 mx-auto md:py-14">
+			<div class="flex flex-wrap w-full mb-20">
+				<h3 class="w-full mt-10 text-3xl font-extrabold tracking-tighter text-center text-gray-400 sm:text-65xl lg:text-4xl lg:mt-0 dark:text-gray-300">
 					Experience
 				</h3>
 			</div>
-			<div class="pt-0 pb-0 sm:items-start mx-auto" v-for="edge in items.edges" :key="edge.node.id">
-				<Experience :item="edge.node" />
+
+			<div class="grid grid-cols-1 gap-8 md:grid-cols-3">
+				<div class="" v-for="edge in items.edges" :key="edge.node.id">
+					<Experience :item="edge.node" />
+				</div>
 			</div>
 		</div>
 	</section>
