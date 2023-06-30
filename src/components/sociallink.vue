50c810e500defeeca900679e29f0d54f3f5842d8
Gareth Wright <garpunkal@gmail.com>
2023-06-30 11:28:03 +0100

    rel

--- src/components/sociallink.vue
+++ src/components/sociallink.vue
@@ -1,5 +1,5 @@
 <template>
-	<a class="inline-flex justify-center link" :href="href" target="_blank" rel="noopener">
+	<a class="inline-flex justify-center link" :href="href" target="_blank" :rel="`noopener ` + rel">
 		<svg xmlns="http://www.w3.org/2000/svg" :class="cssclass || 'h-6 w-6 md:h-7 md:w-7 fill-current text-davy-grey hover:text-ocean dark:text-ocean dark:hover:text-white hover:animate-pulse'" :stroke-width="strokeWidth" :stroke="stroke" :fill="fill || 'currentColor'" role="img" viewBox="0 0 24 24">
 			<title>{{ title }}</title>
 			<path :d="svgPath" :stroke-linecap="strokeLineCap || 'currentColor'" :stroke-linejoin="strokeLineJoin || 'currentColor'" :title="title" />
@@ -18,6 +18,7 @@ export default {
 		strokeLineCap: { type: String },
 		strokeLineJoin: { type: String },
 		cssclass: { type: String },
+		rel: {type:String }
 	},
 };
 </script>
